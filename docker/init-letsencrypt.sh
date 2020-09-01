5746cf6ee78d2f172ca7b4d7753d3ceb93ced6b2
Connor Hollis <connor@hemme.games>
2020-08-31 23:29:35 -0700

    Updating lets encrypt script so we can actually request a cert...

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -5,17 +5,14 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(hemme.games www.hemme.games)
+domains=(www.hemme.games hemme.games )
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
+  exit
 fi
 
 
