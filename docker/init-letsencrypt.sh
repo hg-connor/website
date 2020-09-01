be35bd798b52ac37033f63ccf5a26f83829b726f
Connor Hollis <connor@hemme.games>
2020-09-01 00:23:43 -0700

    Attempting to modify the let's encrypt script to get a cert without waiting a week

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -7,14 +7,13 @@ fi
 
 domains=(hemme.games www.hemme.games)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./data/certbot/conf/live"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
+if [ ${#data_path[@]} -gt 0 ]; then 
   exit
 fi
-
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
